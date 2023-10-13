f2053815202b2fc4f33cec58c5420ddffa85cb51
dennuguyen <dennuguyen@gmail.com>
2023-10-13 20:07:51 +1100

    Clean style.

--- ass1.sql
+++ ass1.sql
@@ -15,42 +15,52 @@
 ---- Q1
 
 create or replace view q1 as
-select region as state,
-       count(*) as nbreweries
+select
+    locations.region as state,
+    count(*) as nbreweries
 from breweries
 inner join locations on locations.id = breweries.located_in
 where locations.country = 'Australia'
-group by region; 
+group by locations.region;
 
 ---- Q2
 
 create or replace view q2 as
-select name as style,
-       min_abv,
-       max_abv
+select
+    name as style,
+    min_abv,
+    max_abv
 from styles
-where max_abv - min_abv = (select max(max_abv - min_abv) from styles);
+where
+    max_abv - min_abv = (
+        select max(max_abv - min_abv) from styles
+    );
 
 ---- Q3
 
 create or replace view q3 as
-select styles.name as style,
-       min(abv) as lo_abv,
-       max(abv) as hi_abv,
-       min_abv,
-       max_abv
+select
+    styles.name as style,
+    min(beers.abv) as lo_abv,
+    max(beers.abv) as hi_abv,
+    styles.min_abv as min_abv,
+    styles.max_abv as max_abv
 from beers
 inner join styles on beers.style = styles.id
-group by styles.name,
-         styles.min_abv,
-         styles.max_abv
-having (min(abv) < min_abv or max(abv) > max_abv) and min_abv != max_abv;
+group by
+    styles.name,
+    styles.min_abv,
+    styles.max_abv
+having
+    (min(beers.abv) < styles.min_abv or max(beers.abv) > styles.max_abv)
+    and styles.min_abv != styles.max_abv;
 
 ---- Q4
 
 create or replace view q4 as
-select breweries.name as brewery,
-       avg(beers.rating)::numeric(3, 1) as rating
+select
+    breweries.name as brewery,
+    avg(beers.rating)::numeric(3, 1) as rating
 from beers
 inner join brewed_by on beers.id = brewed_by.beer
 inner join breweries on brewed_by.brewery = breweries.id
@@ -64,9 +74,10 @@ limit 1;
 
 create or replace function q5(pattern text)
 returns table(beer text, container text, std_drinks numeric) as $$
-    select beers.name,
-           beers.volume || 'ml ' || beers.sold_in as container,
-           (beers.volume * beers.abv * 0.0008)::numeric(3, 1) as std_drinks
+    select
+        beers.name as name,
+        beers.volume || 'ml ' || beers.sold_in as container,
+        (beers.volume * beers.abv * 0.0008)::numeric(3, 1) as std_drinks
     from beers
     where beers.name ilike '%' || pattern || '%';
 $$ language sql;
@@ -75,10 +86,11 @@ $$ language sql;
 
 create or replace function q6(pattern text)
 returns table(country text, first int, nbeers int, rating numeric) as $$
-    select locations.country as country,
-           min(beers.brewed) as first,
-           count(*) as nbeers,
-           avg(beers.rating)::numeric(3, 1) as rating
+    select
+        locations.country as country,
+        min(beers.brewed) as first,
+        count(*) as nbeers,
+        avg(beers.rating)::numeric(3, 1) as rating
     from beers
     inner join brewed_by on brewed_by.beer = beers.id
     inner join breweries on brewed_by.brewery = breweries.id
@@ -92,18 +104,18 @@ $$ language sql;
 create or replace function q7(_beerID int)
 returns text as $$
 declare
-    beerName text;
+    beer_name text;
     ingredient record;
     retval text;
 begin
-    select beers.name into beerName from beers where beers.id = _beerID;
+    select beers.name into beer_name from beers where beers.id = _beerID;
 
-    if beerName is null then
+    if beer_name is null then
         return 'No such beer (' || _beerID || ')';
     end if;
 
     -- Add beer name to returning string.
-    retval := '"' || beerName || '"';
+    retval := '"' || beer_name || '"';
 
     if not exists(select from contains where contains.beer = _beerID) then
         return retval || e'\n' || '  no ingredients recorded';
@@ -112,7 +124,9 @@ begin
     -- Add ingredient names and itype to returning string.
     retval := retval || e'\n' || '  contains:';
     for ingredient in (
-        select ingredients.name, ingredients.itype
+        select
+            ingredients.name,
+            ingredients.itype
         from contains
         inner join ingredients on ingredients.id = contains.ingredient
         where contains.beer = _beerID
@@ -139,17 +153,24 @@ declare
     prev_id int;
 begin
     for beer in (
-        select beers.name as beer,
-               string_agg(distinct breweries.name, '+' order by breweries.name) as brewery,
-               case when ingredients.itype = 'hop' then string_agg(distinct ingredients.name, ',' order by ingredients.name) else 'no hops recorded' end as hops,
-               beers.id as id
+        select
+            beers.id as id,
+            beers.name as beer,
+            string_agg(distinct breweries.name, '+' order by breweries.name) as brewery,
+            case
+                when ingredients.itype = 'hop'
+                then string_agg(distinct ingredients.name, ',' order by ingredients.name)
+                else 'no hops recorded'
+            end as hops
         from beers
         full join contains on beers.id = contains.beer
         full join ingredients on ingredients.id = contains.ingredient
         inner join brewed_by on brewed_by.beer = beers.id
         inner join breweries on brewed_by.brewery = breweries.id
         where beers.name ilike '%' || pattern || '%'
-        group by beers.id, ingredients.itype
+        group by
+            beers.id,
+            ingredients.itype
         order by beers.id
     )
     loop
@@ -172,34 +193,36 @@ create type Collab as (brewery text, collaborator text);
 create or replace function q9(breweryID int)
 returns setof Collab as $$
 declare
-    breweryName text;
+    brewery_name text;
     collabs record;
-    isFirstLoop boolean := true;
+    is_first_loop boolean := true;
 begin
-    select breweries.name into breweryName from breweries where breweries.id = breweryID;
+    select breweries.name into brewery_name from breweries where breweries.id = breweryID;
 
-    if breweryName is null then
+    if brewery_name is null then
         return next ('No such brewery (' || breweryID || ')', 'none')::Collab;
         return;
     end if;
 
     for collabs in (
-        select distinct(breweries.id) as id,
-               breweries.name as name
+        select
+            distinct(breweries.id) as id,
+            breweries.name as name
         from brewed_by
         inner join breweries on brewed_by.brewery = breweries.id
-        -- All beers that specified brewery has worked on.
-        where brewed_by.beer in (
-            select brewed_by.beer from brewed_by where breweryID = brewed_by.brewery
-        )
-        -- To not duplicate specified brewery.
-        and breweryID != brewed_by.brewery
+        where
+            -- All beers that specified brewery has worked on.
+            brewed_by.beer in (
+                select brewed_by.beer from brewed_by where breweryID = brewed_by.brewery
+            )
+            -- To not duplicate specified brewery.
+            and breweryID != brewed_by.brewery
         order by breweries.name
     )
     loop
-        if isFirstLoop then
-            return next (breweryName, collabs.name)::Collab;
-            isFirstLoop := false;
+        if is_first_loop then
+            return next (brewery_name, collabs.name)::Collab;
+            is_first_loop := false;
             continue;
         end if;
 
@@ -207,9 +230,8 @@ begin
     end loop;
 
     -- Collabs is empty.
-    if isFirstLoop then
-        return next (breweryName, 'none')::Collab;
+    if is_first_loop then
+        return next (brewery_name, 'none')::Collab;
     end if;
 end;
 $$ language plpgsql;
-
